f08f9f344959b7a86b9e88887ef29b955b91c35d
Adil Irshad <adil58758@gmail.com>
2022-06-21 20:15:36 +0530

    Cleaned the folders

--- nft-frontend/pages/api/[tokenId].js
+++ nft-frontend/pages/api/[tokenId].js
@@ -4,5 +4,13 @@ export default function handler(req, res) {
 	const tokenId = req.query.tokenId;
 	const name = `Crypto Dev #${tokenId}`;
 	const description = "Crypto Devs are NFTs for Web3 Devs";
-	res.status(200).json({ name: tokenId });
+	const image = `https://raw.githubusercontent.com/adil14788/NFT-Collection/main/nft-frontend/public/cryptodevs/${
+		Number(tokenId) - 1
+	}.svg`;
+
+	return res.status(200).json({
+		name: name,
+		description: description,
+		image: image,
+	});
 }
